60de892091494d8ad08282d4bcce443569fc227a
Inderpreet <>
2018-10-16 23:35:53 -0700

    commit

--- src/tickettoride/TicketToRide.java
+++ src/tickettoride/TicketToRide.java
@@ -16,7 +16,7 @@ public class TicketToRide {
      */
     public static void main(String[] args) {
         // TODO code application logic here frfrfrrvrvr btbtbtbtb
-        // 
+        //  
     } 
     
 }
