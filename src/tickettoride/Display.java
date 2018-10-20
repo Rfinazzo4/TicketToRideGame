73344db5f8a14fa17e061f02af9f79b51c7ac163
Inderpreet <>
2018-10-19 17:59:30 -0700

    Display only has displayBoard as of now

---
+++ src/tickettoride/Display.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tickettoride;
+
+public class Display {
+   
+   
+    // have constructor display the initial board
+    public Display() {
+        
+    }
+ 
+    public void displayBoard() {
+        Board b = new Board();
+        System.out.println(b);
+    }
+}
